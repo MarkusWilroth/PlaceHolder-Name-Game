3255ff10e416ffe0a02fc08818e076c155113a46
Ema Mujezinovic <35938174+eemaaa@users.noreply.github.com>
2018-04-10 10:38:13 +0200

    Skapat klasser för startmenyn

---
+++ Placeholder-Name-Game/PlaceholderGame/PlaceholderGame/StartMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceholderGame {
+    class StartMenu {
+
+        //Start, Options -> ny sida med flerval, Quit
+    }
+}
