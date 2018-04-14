e8aefda9a65b2ad3d34acf334ccb29c88f2f636e
fobian1 <fabbepingis@gmail.com>
2018-04-14 14:01:51 +0200

    PlayerObject

--- Placeholder-Name-Game/PlaceholderGame/PlaceholderGame/Hud.cs
+++ Placeholder-Name-Game/PlaceholderGame/PlaceholderGame/Hud.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PlaceholderGame {
-    class Hud: PlayerObjects {
+    class Hud {
+        int HP, ScreenHeight, ScreenWidth;
+        public SpriteFont hudFont;
+        public Vector2 HPPos;
+        public bool showHud;
 
+        public Hud() {
+
+            HP = HP;
+            ScreenHeight = 900;
+            ScreenWidth = 1600;
+            showHud = true;
+            hudFont = null;
+        }
+            
     }
 }
