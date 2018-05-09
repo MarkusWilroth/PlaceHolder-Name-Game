4ac1396958cf3256d94d03dcec921a3aa5c1413b
Aneela Mariam Latifi <ah7590@mah.se>
2018-05-09 11:48:17 +0200

    klar me alla rektanglar
    skapat rektanglar för all vapen och optionmeynyn
    
    Co-Authored-By: eemaaa <eemaaa@users.noreply.github.com>

--- Placeholder-Name-Game/PlaceholderGame/PlaceholderGame/OptionsMenu.cs
+++ Placeholder-Name-Game/PlaceholderGame/PlaceholderGame/OptionsMenu.cs
@@ -17,25 +17,29 @@ namespace PlaceholderGame {
 
         Rectangle returnRec, twoPlayerRec, threePlayerRec, fourPlayerRec;
         Rectangle bananaRec, waterGunRec, laserSwordRec, baseBallBatRec;
+        Rectangle soundRec;
 
         public OptionsMenu(int nrPlayers, int nrWeapons, int seeWeapons) {
             this.nrPlayers = nrPlayers;
             this.nrWeapons = nrWeapons;
             this.seeWeapons = seeWeapons;
 
-            this.returnRec = returnRec;
-            this.twoPlayerRec = twoPlayerRec;
-            this.threePlayerRec = threePlayerRec;
-            this.fourPlayerRec = fourPlayerRec;
+            this.returnRec = new Rectangle(701,832, 198, 46);
+            this.twoPlayerRec = new Rectangle(714, 733, 49, 49);
+            this.threePlayerRec = new Rectangle(776, 733, 49, 49);
+            this.fourPlayerRec = new Rectangle(839, 733, 49, 49);
 
-            this.bananaRec = bananaRec;
-            this.waterGunRec = waterGunRec;
-            this.laserSwordRec = laserSwordRec;
-            this.baseBallBatRec = baseBallBatRec;
+            this.bananaRec = new Rectangle(727, 259, 49, 49);
+            this.waterGunRec = new Rectangle(835, 259, 49, 49);
+            this.laserSwordRec = new Rectangle(726, 332, 49, 49);
+            this.baseBallBatRec = new Rectangle(835, 334, 49, 49);
+
+            soundRec = new Rectangle(786, 564, 49, 49);
 
         }
 
         public void Update() {
+            
 
         }
 
